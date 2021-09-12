6b929f0934d55db3084b6f3e7eafde5bc373b100
grillow <spmr757@gmail.com>
2021-09-13 00:19:56 +0300

    single-page website

--- frontendServer.js
+++ frontendServer.js
@@ -1,12 +1,20 @@
 'use strict';
 
+const fs = require('fs');
+
 class FrontendServer {
   constructor() {
     this.app = require('express')();
   }
 
   listen(host, port) {
+
+    this.app.get('*', (req, res) => {
+      res.send(fs.readFileSync('index.html', 'utf-8'));
+    });
+
     this.app.listen(port, host);
+
   }
 }
 
