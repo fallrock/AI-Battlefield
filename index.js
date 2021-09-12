9fb055b9f3cca30aee88322601dd9d7cd5ea549f
grillow <spmr757@gmail.com>
2021-09-12 20:22:45 +0300

    ws echo server

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { Server } from './server.js';
+
+const config = {
+  port: process.env.PORT || 8080
+};
+
+const server = new Server(config.port);
---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import { WebSocketServer } from 'ws';
+
+export class Server {
+  constructor(port) {
+    this.wss = new WebSocketServer({ port: port });
+
+    this.wss.on('connection', (ws) => {
+
+      ws.on('message', (message) => {
+        console.log(`message: ${message}`);
+      });
+
+      ws.send('${message}');
+
+    });
+
+  }
+}
