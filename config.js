5a8eee3f1e7546ad0af848bd0520c0df3252c181
grillow <spmr757@gmail.com>
2021-09-13 16:17:27 +0300

    frontend moved to another repo

--- config.js
+++ config.js
@@ -1,14 +1,8 @@
 'use strict';
 
 const config = {
-  backend: {
-    host: process.env.BACKEND_HOST || '0.0.0.0',
-    port: process.env.BACKEND_PORT || 8081
-  },
-  frontend: {
-    host: process.env.FRONTEND_HOST || '0.0.0.0',
-    port: process.env.BACKEND_PORT || 8080
-  }
+  host: process.env.BACKEND_HOST || '0.0.0.0',
+  port: process.env.BACKEND_PORT || 8081
 };
 
 module.exports = { config };
--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 
 const { config } = require('./config.js');
 const { GameServer } = require('./gameServer.js');
-const { FrontendServer } = require('./frontendServer.js');
 
 const gameServer = new GameServer();
-gameServer.listen(config.backend.host, config.backend.port);
-
-const frontendServer = new FrontendServer();
-frontendServer.listen(config.frontend.host, config.frontend.port);
+gameServer.listen(config.host, config.port);
