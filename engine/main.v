def7165dfcbb4bb92564413e1ebfdd57512e036f
Dmitry <deekovalev@gmail.com>
2021-10-15 18:48:41 +0300

    db

--- engine/main.v
+++ engine/main.v
@@ -7,10 +7,23 @@ import time
 import json
 import export_ai
 import export_render
+import sqlite
+
+struct AiRecord {
+	id  int    [primary; sql: serial]
+	uid string [unique]
+	code  string
+}
+
 
 fn main() {
 	mut e := engine.Engine{
 		delta_time: 1.0 / 10
+	}
+
+	ai_db := sqlite.connect(':memory:') ?
+	sql ai_db {
+		create table AiRecord
 	}
 
 	mut rest_srv := rest.new_server(8082)
@@ -22,23 +35,34 @@ fn main() {
 	mut runner := runner.new_runner(['/usr/bin/node', 'API/AI-Runner/runner.js'])
 	runner.start()
 
-	e.create_drone()
+	uid := e.create_drone()
+	test_ai := AiRecord{
+		uid: uid.str()
+		code: os.read_file('cli/example-ai/rpatrol.js') or { panic(err) }
+	}
+	sql ai_db {
+		insert test_ai into AiRecord
+	}
 
 	for {
 		println('tick')
-		apply_ai(mut e, mut runner)
+		apply_ai(mut e, mut runner, ai_db)
 		e.on_tick()
 		ws_srv.broadcast(export_render.encode(e)) ?
 		time.sleep(e.delta_time * time.second)
 	}
 }
 
-fn apply_ai(mut e engine.Engine, mut runner runner.Runner) {
+fn apply_ai(mut e engine.Engine, mut runner runner.Runner, ai_db sqlite.DB) {
 	for mut d in e.drones {
+		uid := d.id.str()
+		ai_rec := sql ai_db {
+			select from AiRecord where uid == uid limit 1
+		}
 		runner_inp := export_ai.encode(
 			id: d.id
 			engine: e
-			ai: os.read_file('cli/example-ai/rpatrol.js') or { panic(err) }
+			ai: ai_rec.code
 		)
 		runner_out := runner.process(runner_inp)
 
