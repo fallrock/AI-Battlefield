27a4e290a96f1505711f1b979df808c51e3ae9d8
Dmitry <deekovalev@gmail.com>
2021-10-15 17:22:01 +0300

    abstract v runner

--- engine/main.v
+++ engine/main.v
@@ -2,7 +2,10 @@ import engine
 import streamer
 import rest
 import runner
+import os
 import time
+import json
+import export_ai
 import export_render
 
 fn main() {
@@ -16,15 +19,21 @@ fn main() {
 	mut ws_srv := streamer.new_server(8081) ?
 	go ws_srv.listen()
 
-	mut runner := runner.new_runner()
+	mut runner := runner.new_runner(['/usr/bin/node', 'API/AI-Runner/runner.js'])
 	runner.start()
 
 	e.create_drone()
 
 	for {
 		println('tick')
-		e.on_tick(fn (id engine.Uid, e engine.Engine, mut runner &runner.Runner) ?engine.AiState {
-			return runner.exec(id, e)
+		e.on_tick(fn (id engine.Uid, e engine.Engine, mut runner runner.Runner) ?engine.AiState {
+			inp := export_ai.encode(
+				id: id
+				engine: e
+				ai: os.read_file('cli/example-ai/rpatrol.js') or { panic(err) }
+			)
+			out := runner.process(inp)
+			return json.decode(engine.AiState, out) or {}
 		}, &runner)
 		ws_srv.broadcast(export_render.encode(e)) ?
 		time.sleep(e.delta_time * time.second)
--- engine/runner/runner.v
+++ engine/runner/runner.v
@@ -1,19 +1,16 @@
 module runner
 
-import engine
 import os
-import json
 import term
-import export_ai
 
-struct Runner{
+struct Runner {
 	os.Process
 }
 
-pub fn new_runner() Runner {
-	mut proc := os.new_process('/usr/bin/node')
+pub fn new_runner(argv []string) Runner {
+	mut proc := os.new_process(argv.first())
 	proc.set_redirect_stdio()
-	proc.set_args(['API/AI-Runner/runner.js'])
+	proc.set_args(argv[1..])
 	return Runner{proc}
 }
 
@@ -25,22 +22,17 @@ pub fn (mut runner Runner) stop() {
 	runner.close()
 }
 
-pub fn (mut runner Runner) exec(id engine.Uid, e engine.Engine) ?engine.AiState {
-		runner_data := export_ai.encode(
-			id: id
-			engine: e
-			ai: os.read_file('cli/example-ai/rpatrol.js') or { panic(err) }
-		)
-		runner.stdin_write(runner_data)
-		runner.stdin_write('\n')
-		result := runner.stdout_read()
-		dump(runner_data)
-		dump(result)
-		if !runner.is_alive() {
-			println(term.header_left('RUNNER STDERR', '-'))
-			println(term.red(runner.stderr_read()))
-			println(term.h_divider('-'))
-			panic('runner died')
-		}
-		return json.decode(engine.AiState, result)
+pub fn (mut runner Runner) process(data string) string {
+	runner.stdin_write(data)
+	runner.stdin_write('\n')
+
+	result := runner.stdout_read()
+
+	if !runner.is_alive() {
+		println(term.header_left('RUNNER STDERR', '-'))
+		println(term.red(runner.stderr_read()))
+		println(term.h_divider('-'))
+		panic('runner died')
+	}
+	return result
 }
