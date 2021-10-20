b179dd2519de62153508b05d6b3fc646d44649e0
Dmitry <deekovalev@gmail.com>
2021-10-20 20:56:34 +0300

    cleanup

--- engine/main.v
+++ engine/main.v
@@ -1,3 +1,5 @@
+module main
+
 import engine
 import streamer
 import rest
@@ -5,36 +7,17 @@ import runner
 import os
 import time
 import export_render
-import app as app_module
-import sqlite
-
-fn example_init(mut app app_module.AppState) {
-	sql app.ai_db {
-		create table app_module.AiRecord
-	}
-
-	uid := app.e.create_drone()
-	test_ai := app_module.AiRecord{
-		uid: uid.str()
-		code: os.read_file('cli/example-ai/rpatrol.js') or { panic(err) }
-	}
-	sql app.ai_db {
-		insert test_ai into app_module.AiRecord
-	}
-}
+import app
 
 fn main() {
-	if 'PROJECT_ROOT' in os.environ() {
-		os.chdir(os.environ()['PROJECT_ROOT']) or { panic(err) }
-	}
-	mut app := app_module.AppState{
+	os.chdir(os.environ()['PROJECT_ROOT']) or { panic(err) }
+
+	mut app := app.AppState{
 		e: engine.Engine{
 			delta_time: 1.0 / 10
 		}
-		ai_db: sqlite.connect(':memory:') ?
+		ai_db: mk_mem_db()
 	}
-
-	example_init(mut app)
 
 	mut rest_srv := rest.new_server(rest.Handler{&app}, 8082)
 	go rest_srv.listen_and_serve()
@@ -46,11 +29,9 @@ fn main() {
 	runner.start()
 
 	for {
-		// println('tick')
 		app.apply_ai(mut runner)
 		app.e.on_tick()
 		ws_srv.broadcast(export_render.encode(app.e)) ?
 		time.sleep(app.e.delta_time * time.second)
 	}
 }
-
